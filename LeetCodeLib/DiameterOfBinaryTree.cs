e14b04e9bb185f806034d3fc0f766754eaac5aef
jedi <jedi2454@gmail.com>
2023-05-12 20:12:46 +0800

    Diameter BT

--- LeetCodeLib/DiameterOfBinaryTree.cs
+++ LeetCodeLib/DiameterOfBinaryTree.cs
@@ -5,17 +5,27 @@ using System.Threading.Tasks;
 
 namespace LeetCodeLib
 {
+    /// <summary>
+    /// Ref by https://www.youtube.com/watch?v=3vZV-6qPDmE
+    /// DFS with tracking
+    /// </summary>
     public static class DiameterOfBinaryTree
     {
+        static int MaxDiameter = 0;
         public static int GetDiameterOfTree(TreeNode root)
         {
-            return 0;
+            MaxDiameter = 0;
+            GetHeight(root);
+            return MaxDiameter;
         }
 
         public static int GetHeight(TreeNode node)
         {
             if (node == null) return 0;
-            return 1 + Math.Max(GetHeight(node.left), GetHeight(node.right));
+            int lH = GetHeight(node.left);
+            int rH = GetHeight(node.right);
+            MaxDiameter = Math.Max(MaxDiameter, lH + rH); // lH + rH
+            return 1 + Math.Max(lH, rH);
         }
     }
 }
--- LeetCodeTest/UnitTest2.cs
+++ LeetCodeTest/UnitTest2.cs
@@ -6,11 +6,19 @@ namespace LeetCodeTest;
 public class UnitTest2
 {
     [TestMethod]
-    public void Template()
+    public void DiameterOfBinaryTreeTest()
     {
         int?[] root = { 1, 2, 3, 4, 5 };
         BinaryTree tree = new BinaryTree(root);
-        tree.InOrderTraversal(tree.root);
+        Assert.IsTrue(DiameterOfBinaryTree.GetDiameterOfTree(tree.root) == 3);
+    }
+
+    [TestMethod]
+    public void DiameterOfBinaryTreeTest2()
+    {
+        int?[] root = { 1, 2 };
+        BinaryTree tree = new BinaryTree(root);
+        Assert.IsTrue(DiameterOfBinaryTree.GetDiameterOfTree(tree.root) == 1);
     }
 
 }
